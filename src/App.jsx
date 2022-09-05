8fc1bc84c3f0e3c49c0607343f0d8ea773a1f739
resourcefulmind <resourcefulmindoso@gmail.com>
2022-09-05 18:05:31 +0100

    chore: initialize components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 
+import { Navbar, Hero,  Billing, Business, Button, CardDeal, Clients, CTA, Footer, Stats, Testimonials } from './components'
 import styles from './style'
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}> 
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}> 
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
