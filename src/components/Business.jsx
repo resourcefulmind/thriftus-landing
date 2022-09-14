7946b2da350e92e89eea8f57fb9718e19bb1bdd6
resourcefulmind <resourcefulmindoso@gmail.com>
2022-09-14 11:16:16 +0100

    chore: implement business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { features } from '../constants';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div>
+    <div>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain' />
+    </div>
+  </div>
+)
+
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Create, <br className='sm:block hidden' /> automate and monitor <br className='sm:block hidden' /> your contributions with ease.</h2>
+        <p className={`${styles.paragraph} max-w-[670px] mt-5`}>Complete your KYC verification, Create a new group for you and your buddies to start making daily, weekly or monthly contributions and let us handle the rest. Don't have buddies or want to make a bigger contribution? Our system will match you with other people having the same contribution targets at any time. Our system speeds up the process of requesting, verifying and disbursing contributions. </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => {
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        })}
+      </div>
+    </section>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <motion.button
+      whileInView={{ opacity: 1 }}
+      whileHover={{ scale: 1.2 }}
+      transition={{ duration: 0.5, type: 'tween' }}
+      className={`py-4 px-6 bg-blue-gradient font-medium font-poppins rounded-[10px] w-[170px] text-[18px] text-primary outline-none ${styles}`}
+      type='button'
+    >
+      Get Started
+    </motion.button>
   )
 }
 
