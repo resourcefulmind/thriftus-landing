4af81cb11f90a0a57abc066224d6e764a0c7fbb8
resourcefulmind <resourcefulmindoso@gmail.com>
2022-09-19 17:06:05 +0100

    chore: implement call-to-action

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from '../style';
+import Button from './Button';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className='flex-1 flex flex-col'>
+      <h2 className={styles.heading2}>Achieve your financial goals with ease</h2>
+      <p className={`${styles.paragraph} max-w-[870px] mt-5`}>Let us help you reach your targets and meet your economic needs with less headaches and in less time. ThriftUs uses the highest level of Internet Security and it is secured by 256 bits SSL security encryption.</p>
+    </div>
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+)
 
 export default CTA
